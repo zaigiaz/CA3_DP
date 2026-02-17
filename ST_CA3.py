afa12292fa74436f600e94ad5312f4fe7ceff01a
zaigiaz <shayne.tman@gmail.com>
2026-02-16 23:11:57 -0600

    basic work

---
+++ ST_CA3.py
@@ -0,0 +1,29 @@
+# [Name here]
+# Undergrad
+
+import random
+
+# size of card deck
+n = 10 
+
+# Names of the children/grandchildren
+names = ["maria", "andrea", "jason"]
+
+# cards with values associated
+cards = {}
+
+# Dynamic Programming Table to compute sub-problems
+dp_table = [0] * (n+1)
+
+
+print("hello")
+
+
+def choose_child():
+    print("random choice of child")
+    # use random.choice for this
+
+
+def print_output():
+    print("Final-Output:\n")
+    # print output of program
